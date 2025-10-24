c05bf6fac28888c3e518438f02f2d28e78a1f27b
LucyCat <78803519+LucyCat-coder@users.noreply.github.com>
2025-10-24 19:50:13 +0300

    1

--- my_study.py
+++ my_study.py
@@ -1,2 +1,10 @@
-print("Hello world!")
-
+salary = float(input("Укажите вашу заработную плату в месяц: "))
+hypothec_percent = float(input("Укажите, какой процент(%) уходит на ипотеку: "))
+life_percent = float(input("Укажите, какой процент(%) уходит на жизнь: "))
+time_period = int(input("За какой временной отрезок в месяцах нужно рассчитать: "))
+hypothec_money = (salary * hypothec_percent / 100) * time_period
+life_money = (salary * life_percent / 100) * time_period
+saved_money = salary * time_period - (hypothec_money + life_money)
+print("На ипотеку было потрачено: ", hypothec_money)
+print("На жизнь было потрачено: ", life_money)
+print("Было накоплено: ", saved_money)
